f854fe84b109500b073998741a7210c6cbdcb85f
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-24 14:43:42 +0100

    Create EnemyController that moves towards players position at instantiation

---
+++ Assets/scripts/EnemyController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	[SerializeField]
+	private PlayerController player;
+	[SerializeField]
+	private float moveSpeed = 0.5f;
+	private Vector2 direction;
+
+	private void Start () {
+		direction = player.transform.position - transform.position;
+	}
+
+	private void Update () {
+		transform.Translate (direction * moveSpeed * Time.deltaTime);
+	}
+
+	private void OnTriggerEnter2D (Collider2D trig) {
+		if (trig.tag == "projectile") {
+			Destroy (gameObject);
+		}
+	}
+}
