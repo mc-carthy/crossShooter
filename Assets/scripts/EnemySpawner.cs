eb4c610c55433fe71a1ee0baec4ad2f4f66f9013
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-25 09:22:22 +0100

    Spawn enemies at corners

---
+++ Assets/scripts/EnemySpawner.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	[SerializeField]
+	private EnemyController enemy;
+	[SerializeField]
+	private float spawnRate = 2f;
+	private bool canSpawn = true;
+	private Vector3[] spawnPositions = new Vector3[] {
+		new Vector3 (10, 6, 0),
+		new Vector3 (-10, 6, 0),
+		new Vector3 (10, -6, 0),
+		new Vector3 (-10, -6, 0),
+	};
+
+	private void Start () {
+		StartCoroutine (SpawnEnemies(spawnRate));
+	}
+
+	private void SpawnEnemiesAtCorners() {
+		foreach (Vector3 pos in spawnPositions) {
+			EnemyController newEnemy = Instantiate (
+				enemy,
+				pos,
+				Quaternion.identity
+			) as EnemyController;
+			newEnemy.transform.SetParent (this.transform);
+		}
+	}
+
+	private IEnumerator SpawnEnemies (float spawnRate) {
+		while (canSpawn) {
+			yield return new WaitForSeconds (spawnRate);
+			SpawnEnemiesAtCorners ();
+		}
+	}
+}
